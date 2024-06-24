b90761a5cd7463d9a8b7d8b7d4b588c3da5a3282
Ерол Мандажиев <134313569+erolcoo@users.noreply.github.com>
2024-06-24 18:37:39 +0300

    Add files via upload

---
+++ Intro to Node.js/Lecture Viktor/intro/index.js
@@ -0,0 +1,4 @@
+const { print, add, data } = require('./util.js');
+
+print('Hello world! ' + add(2, 2));
+print(data[2]);
---
+++ Intro to Node.js/Lecture Viktor/intro/util.js
@@ -0,0 +1,15 @@
+function print(msg){
+    console.log(msg);
+}
+
+function add (a, b){
+    return a + b;
+}
+
+let data = [1, 2, 3];
+
+module.exports = {
+    print, 
+    add,
+    data
+};
